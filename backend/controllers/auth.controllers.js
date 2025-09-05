cab0e7c1988c95fd2f8ab932edfa7a7b72591bd4
nitinbutnvm <heynittinnn@gmail.com>
2025-09-05 17:23:54 +0530

    feat: implement authentication routes and controllers for sign up, login, and logout

---
+++ backend/controllers/auth.controllers.js
@@ -0,0 +1,87 @@
+import genToken from '../config/token.js'
+import  User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
+
+export const signUp = async (req, res) => {
+try {
+    const { name, email, password }= req.body  
+
+    const existEmail = await User.findOne({email})
+
+    if(existEmail){
+        return res.status(400).json({message: 'Email already exists'})
+    } 
+
+    if(password.length < 6){
+        return res.status(400).json({message: 'Password must be at least 6 characters'})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })    
+    const token = await genToken(user._id)
+    res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+        sameSite: "strict",
+        secure : false
+    })  
+    
+    return res.status(201).json(user)
+} catch (error) {
+    return res.status(500).json({message: `sign up failed: ${error} `})   
+}
+
+
+
+}
+
+export const login = async (req, res) => {
+try {
+    const {email, password }= req.body  
+
+    const user = await User.findOne({email})
+
+    if(!user){
+        return res.status(400).json({message: 'Email does not exists'})
+    } 
+
+    const isMatch = await bcrypt.compare(password, user.password)
+   
+    if(!isMatch){
+        return res.status(400).json({message: 'Invalid credentials'})
+    }
+
+
+
+    const token = await genToken(user._id)
+    res.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 10 * 24 * 60 * 60 * 1000, // 10 days
+        sameSite: "lax",
+        secure : false
+    })  
+    
+    return res.status(200).json(user)
+
+} catch (error) {
+    return res.status(500).json({message: `login failed: ${error} `})   
+}
+
+
+
+}
+
+export const logOut = async (req, res) => {
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message: 'Logged out successfully'})
+        }  
+ catch (error) {
+        return res.status(500).json({message: `logout failed: ${error} `})   
+    }                       
+}
