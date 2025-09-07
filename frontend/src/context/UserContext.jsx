35d2811c79fda54918e0344cab1725cb2a592cd7
nitinbutnvm <heynittinnn@gmail.com>
2025-09-07 12:46:35 +0530

    added backend link

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useEffect, useState } from "react";
 export const userDataContext = createContext();
 
 function UserContext({children}) {
-    const serverUrl = "http://localhost:8000"
+    const serverUrl = "https://virtual-assistant-backend-oug4.onrender.com"
     const [userData, setUserData] = useState(null)
 const [frontendImage, setFrontendImage]=useState(null)
   const [backendImage, setBackendImage]=useState(null)
